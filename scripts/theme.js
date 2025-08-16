fd14ba03de61ee1121d2243967f341896800251e
TheLisper <ntsizwankabane47@gmail.com>
2025-08-16 12:06:35 +0200

    Adding event listeners to theme toggles

--- scripts/theme.js
+++ scripts/theme.js
@@ -34,3 +34,35 @@ function applyTheme(isDark) {
         circle.classList.toggle("theme-dark-clicked", isDark)
     );
 }
+// Add click event listeners to toggle buttons
+// Toggle theme and save preference
+export function setupThemeToggleButtons(){
+    const body = document.body;
+
+    const lightLogo = document.getElementById("logo");
+    const darkLogo = document.getElementById("dark-logo");
+
+    const switchCircles = document.querySelectorAll(".switch-circle");
+    const toogleButtons = document.querySelectorAll(".toggle-theme-btn");
+
+    toogleButtons.forEach(button =>
+        button.addEventListener("click", () => {
+            //Toggle dark class on body and save
+            const isNowDark = body.classList.toggle("dark");
+            localStorage.setItem("theme", isNowDark ? "dark" : "light");
+
+            //show or hide logo based on theme
+            if (lightLogo){
+                darkLogo.style.display = isNowDark? "none" : "flex";
+            }
+            if (darkLogo){
+                darkLogo.style.display = isNowDark ? "flex" : "none";
+            }
+
+            //Toggle switch circle position based on theme
+            switchCircles.forEach(circle =>
+                circle.classList.toggle("theme-dark-clicked", isNowDark)
+            );
+        })
+    );
+}
